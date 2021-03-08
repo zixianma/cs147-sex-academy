1fc6c0537d6a8ef0904e73399171d079345d3ce5
sheikh-srijon <sheikhsrijon@gmail.com>
2021-03-08 20:06:58 +0600

    First commit

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './components/Home';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" /> */}
+      {/* <Home></Home> */}
+
+      {/* <Login></Login> */}
+      <SignUp></SignUp>
+      
     </View>
   );
 }
